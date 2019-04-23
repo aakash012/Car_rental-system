9175c5472d86d5d623ffe169d1e1b5f6ab1be116
aakash012 <aakash012@live.com>
2019-04-23 11:23:48 +0530

    fourth commit

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,6 +5,6 @@
     <div class="jumbotron text-center">
         <h1>About the Project</h1>
         <p>This is a Car Rental service simulation task implemented in Laravel 5.8 and php 7.2</p>    
-    <h1>Created by <br><a href="https://github.com/aakash012">Aakash</a></h1>
+    <h1>Created by <br><a href="https://github.com/aakash012">Aakash, Aman and Manoj</a></h1>
     </div>
 @endsection
